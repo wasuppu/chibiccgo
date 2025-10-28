22268c5f35fd76439feab4e07b0a1670ce9eb13b
lundrore <101697913+lundrore@users.noreply.github.com>
2025-10-28 17:24:26 +0800

    [234] Define __STDC_UTF_{16,32}__ macros

--- preprocess.go
+++ preprocess.go
@@ -986,6 +986,8 @@ func (a X64) initMacro() {
 	defineMacro("__STDC_NO_COMPLEX__", "1")
 	defineMacro("__STDC_NO_THREADS__", "1")
 	defineMacro("__STDC_NO_VLA__", "1")
+	defineMacro("__STDC_UTF_16__", "1")
+	defineMacro("__STDC_UTF_32__", "1")
 	defineMacro("__STDC_VERSION__", "201112L")
 	defineMacro("__STDC__", "1")
 	defineMacro("__USER_LABEL_PREFIX__", "")
