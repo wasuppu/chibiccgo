460283a0d1bec66fbc2d0493fde713adaf556400
lundrore <101697913+lundrore@users.noreply.github.com>
2025-10-28 17:24:18 +0800

    [175] Allow parenthesized expressions as macro arguments

--- test/macro.c
+++ test/macro.c
@@ -229,6 +229,13 @@ int main() {
 #define M8(x,y) x y
   assert(9, M8(, 4+5), "M8(, 4+5)");
 
+  // c175
+#define M8(x,y) x*y
+  assert(20, M8((2+3), 4), "M8((2+3), 4)");
+
+#define M8(x,y) x*y
+  assert(12, M8((2,3), 4), "M8((2,3), 4)");
+
   printf("OK\n");
   return 0;
 }
